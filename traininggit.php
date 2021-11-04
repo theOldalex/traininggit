9ce2b8ce0572198df45c68cd9524c63883fe3ba5
theOldalex <alexandrediakitepro@gmail.com>
2021-11-04 13:58:57 +0100

    Petite modif

--- traininggit.php
+++ traininggit.php
@@ -4,4 +4,5 @@ echo "salut tout le monde je vous rejoint";
 echo "Je me joint a vous";
 echo ('Bienvenue sur la branche Alexandre');
 echo ('Merci,super invitation,c\'est la teuff');
-echo ("djemil trop chaud pour gagné beaucoup de pépétte");
+echo ("djemil trop chaud pour gagné beaucoup d\'argent");
+echo ('Le rer d c\'est génial !');
