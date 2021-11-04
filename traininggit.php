2bc48768394f670ce7d19cebc764442305880b38
aminou aminou sam djemil <djemil.aminou@gmail.com>
2021-11-04 12:49:46 +0000

    nouveau projet sur nouvelle branche jay

--- traininggit.php
+++ traininggit.php
@@ -2,5 +2,6 @@
 
 echo "salut tout le monde je vous rejoint";
 echo "Je me joint a vous";
-echo('Bienvenue sur la branche Alexandre');
-echo('Merci,super invitation,c\'est la teuff');
+echo ('Bienvenue sur la branche Alexandre');
+echo ('Merci,super invitation,c\'est la teuff');
+echo ("djemil trop chaud pour gagné beaucoup de pépétte");
