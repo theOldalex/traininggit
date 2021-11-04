5ea5c58cd0ff1040da75e78f8520a007b266c793
aminou aminou sam djemil <djemil.aminou@gmail.com>
2021-11-04 12:45:27 +0000

    correction sur projet Issam

--- traininggit.php
+++ traininggit.php
@@ -3,8 +3,4 @@
 echo "salut tout le monde je vous rejoint";
 echo "Je me joint a vous";
 echo('Bienvenue sur la branche Alexandre');
-echo('Merci,super invitation,c\'est la tuff')
-
-
-
-?>
+echo('Merci,super invitation,c\'est la teuff');
