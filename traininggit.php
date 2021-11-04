21a980bf02a98e733d9524d2f0a22405e8928953
theOldalex <alexandrediakitepro@gmail.com>
2021-11-04 11:57:21 +0100

    Commit

--- traininggit.php
+++ traininggit.php
@@ -1,13 +1,10 @@
 <?php
 
 echo('Bienvenue sur la branche Alexandre');
-<<<<<<< HEAD
 echo "salut tout le monde je vous rejoint";
 echo "Je me joint a vous";
-=======
-echo('Merci,super invitation,c\'est la tuff')c
+echo('Merci,super invitation,c\'est la tuff')
 
->>>>>>> origin/issam
 
 
 ?>
