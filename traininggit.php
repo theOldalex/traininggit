e9800bb636fee122dfa45fdfe1bb611cc1307d8b
issam <isnahali@numericable.fr>
2021-11-04 14:15:32 +0100

    commit mise à jour  trainingit.php

--- traininggit.php
+++ traininggit.php
@@ -4,3 +4,4 @@ echo "salut tout le monde je vous rejoint";
 echo "Je me joint a vous";
 echo('Bienvenue sur la branche Alexandre');
 echo('Merci,super invitation,c\'est la teuff');
+echo('on va en manger,des git status');
